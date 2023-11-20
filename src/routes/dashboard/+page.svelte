2300c59a7751482f577ec9c661f3375938db9a1c
harshdev2 <averageworkspace@gmail.com>
2023-11-20 16:23:30 +0530

    commit

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,68 +4,141 @@
 		TableHead,
 		TabItem,
 		TableBody,
+		Button,
+		Input,
+		Checkbox,
+		Select,
+		Label,
 		TableBodyCell,
 		TableHeadCell,
 		TableBodyRow,
-		TableSearch
+		TableSearch,
+		Modal,
+		Datepicker
 	} from 'flowbite-svelte';
 
 	let searchTerm = '';
 
-    let color = "green";
+	let color = 'green';
 
-    class Genders {
-        static male = 0;
-        static female = 1;
-    }
+	class Genders {
+		static male = 0;
+		static female = 1;
+	}
 
 	let students = [
-    {
-        name: "Kwame Nkrumah",
-        gender: Genders.male,
-        paymentStatus: true,
-        index: 1023,
-        createdAt: new Date("2023-10-01"),
-    },
-    {
-        name: "Ama Serwaa",
-        gender: Genders.female,
-        paymentStatus: false,
-        index: 1024,
-        createdAt: new Date("2023-10-02"),
-    },
-    {
-        name: "Kofi Annan",
-        gender: Genders.male,
-        paymentStatus: true,
-        index: 1025,
-        createdAt: new Date("2023-10-03"),
-    },
-    {
-        name: "Abena Osei",
-        gender: Genders.female,
-        paymentStatus: false,
-        index: 1026,
-        createdAt: new Date("2023-10-04"),
-    },
-    {
-        name: "Yaw Asamoah",
-        gender: Genders.male,
-        paymentStatus: true,
-        index: 1027,
-        createdAt: new Date("2023-10-05"),
-    }
-];
-
+		{
+			name: 'Kwame Nkrumah',
+			gender: Genders.male,
+			paymentStatus: true,
+			index: 1023,
+			createdAt: new Date('2023-10-01')
+		},
+		{
+			name: 'Ama Serwaa',
+			gender: Genders.female,
+			paymentStatus: false,
+			index: 1024,
+			createdAt: new Date('2023-10-02')
+		},
+		{
+			name: 'Kofi Annan',
+			gender: Genders.male,
+			paymentStatus: true,
+			index: 1025,
+			createdAt: new Date('2023-10-03')
+		},
+		{
+			name: 'Abena Osei',
+			gender: Genders.female,
+			paymentStatus: false,
+			index: 1026,
+			createdAt: new Date('2023-10-04')
+		},
+		{
+			name: 'Yaw Asamoah',
+			gender: Genders.male,
+			paymentStatus: true,
+			index: 1027,
+			createdAt: new Date('2023-10-05')
+		}
+	];
 
 	$: filteredItems = students.filter(
 		(item) => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
 	);
+
+	let addStudentModalOpened = false;
 </script>
 
 <div class="px-12 py-8">
 	<div>
-		<h1 class="text-xl font-semibold">Students List</h1>
+		<div class="flex flex-row justify-between">
+			<h1 class="text-xl font-semibold">Students List</h1>
+			<Button
+				on:click={() => {
+					addStudentModalOpened = true;
+				}}
+				color={'green'}>Add Student</Button
+			>
+		</div>
+
+		<Modal bind:open={addStudentModalOpened} size="xs" autoclose={false} class="w-full">
+			<form class="flex flex-col space-y-6" action="#">
+				<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add User</h3>
+				<Label class="space-y-2">
+					<span>Full Name</span>
+					<Input color={'green'} type="email" name="email" placeholder="E.g. Ama Serwaa" required />
+				</Label>
+				<Label class="space-y-2">
+					<span>Gender</span>
+					<Select
+						defaultClass="text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500 "
+						items={[
+							{ value: 'male', name: 'Male' },
+							{ value: 'female', name: 'Female' },
+							{ value: 'other', name: 'Other' }
+						]}
+					/>
+				</Label>
+
+				<Label class="space-y-2 flex flex-col gap-2">
+					Date of Birth
+					<input
+						class="text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
+						type="date"
+					/>
+				</Label>
+
+				<Label class="space-y-2">
+					<span>Index Number</span>
+					<Input
+						color={'green'}
+						type="email"
+						name="email"
+						placeholder="E.g. xxxxxxxxxx23"
+						required
+					/>
+				</Label>
+
+				<Label class="space-y-2">
+					<span>Email</span>
+					<Input
+						color={'green'}
+						type="email"
+						name="email"
+						placeholder="E.g. test@gmail.com"
+						required
+					/>
+				</Label>
+
+				<Label class="space-y-2">
+					<Input color={'green'} type="text" name="phone" placeholder="E.g. 0541234567" required />
+				</Label>
+
+				<Button color="green" type="submit" class="w-full1">Confirm</Button>
+			</form>
+		</Modal>
 
 		<Table divClass="mt-4" hoverable={true}>
 			<TableHead>
@@ -81,14 +154,12 @@
 			<TableBody tableBodyClass="divide-y">
 				{#each students as student}
 					<TableBodyRow>
-                        <TableBodyCell>{student.index}</TableBodyCell>
+						<TableBodyCell>{student.index}</TableBodyCell>
 						<TableBodyCell>{student.name}</TableBodyCell>
-						<TableBodyCell>{student.gender == Genders.male ? "Male" : "Female"}</TableBodyCell>
-						<TableBodyCell>{student.paymentStatus ? "Completed" : "Pending"}</TableBodyCell>
+						<TableBodyCell>{student.gender == Genders.male ? 'Male' : 'Female'}</TableBodyCell>
+						<TableBodyCell>{student.paymentStatus ? 'Completed' : 'Pending'}</TableBodyCell>
 						<TableBodyCell>
-							<a
-								class="font-medium text-green-600 hover:underline dark:text-green-500">Edit</a
-							>
+							<a class="font-medium text-green-600 hover:underline dark:text-green-500">Edit</a>
 						</TableBodyCell>
 					</TableBodyRow>
 				{/each}
--- src/routes/placement/+page.svelte
+++ src/routes/placement/+page.svelte
@@ -1,32 +1,43 @@
 <script>
-	import { Button, Input, Modal, Alert } from 'flowbite-svelte';
+	import {
+		Button,
+		Input,
+		Modal,
+		Alert,
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
 	let number = '';
 
 	let popupOpen = false;
+	let infoOpen = false;
 
 	let color = 'green';
 </script>
 
-<div
-	class="bg-background text-white flex flex-col min-h-[100vh] items-center justify-center"
->
+<div class="bg-background text-white flex flex-col min-h-[100vh] items-center justify-center">
 	<div
 		class="flex flex-col min-h-[50vh] lg:min-h-[50vh] items-center p-8 rounded-md bg-muted-background"
 	>
-		<h1 class="text-2xl text-[#1aa841] font-semibold mb-4">PEKI SENIOR HIGH SCHOOL</h1>
-		<img class="max-h-[20vh]" src={'/favicon.png'} alt="School Logo" />
-		<h1 class="text-xl text-black mt-6 font-bold text-center">Student's Login</h1>
-		<div class="w-4/5 mt-4 flex flex-col">
-            <span class="text-[15px] my-1 text-white">Index Number</span>
-			<Input
-				bind:value={number}
-				color="green"
-				defaultClass="w-full"
-				placeholder="E.g. xxxxxxxxxx23"
-			/>
-			<span class="text-[14px] mt-1 text-gray-400">Add the year you completed JHS</span>
-		</div>
-		<!-- <Alert color="green">
+		{#if !infoOpen}
+			<h1 class="text-2xl text-[#1aa841] font-semibold mb-4">PEKI SENIOR HIGH SCHOOL</h1>
+			<img class="max-h-[20vh]" src={'/favicon.png'} alt="School Logo" />
+			<h1 class="text-xl text-black mt-6 font-bold text-center">Student's Login</h1>
+			<div class="w-4/5 mt-4 flex flex-col">
+				<span class="text-[15px] my-1 text-white">Index Number</span>
+				<Input
+					bind:value={number}
+					color="green"
+					defaultClass="w-full"
+					placeholder="E.g. xxxxxxxxxx23"
+				/>
+				<span class="text-[14px] mt-1 text-gray-400">Add the year you completed JHS</span>
+			</div>
+			<!-- <Alert color="green">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				fill="currentColor"
@@ -43,15 +54,20 @@
 
 			Add the year you completed JHS.
 		</Alert> -->
-		<div class="mt-6 w-full flex flex-row justify-center">
-			<Button
-				on:click={() => {
-					popupOpen = true;
-				}}
-				class="w-3/5"
-				color="green">Login</Button
-			>
-		</div>
+			<div class="mt-6 w-full flex flex-row justify-center">
+				<Button
+					on:click={() => {
+						if (number == '99') {
+							infoOpen = true;
+						} else {
+							popupOpen = true;
+						}
+					}}
+					class="w-3/5"
+					color="green">Login</Button
+				>
+			</div>
+		{/if}
 
 		<Modal title="Invalid Number" bind:open={popupOpen} size="sm" autoclose>
 			<div class="text-base leading-relaxed">
@@ -61,5 +77,57 @@
 				<Button color="green">Confirm</Button>
 			</svelte:fragment>
 		</Modal>
+
+		{#if infoOpen}
+			<div class="flex flex-col">
+				<div class="flex flex-row">
+					<div>
+						<Table divClass="mt-4" hoverable={true}>
+							<TableHead>
+								<TableHeadCell>Name</TableHeadCell>
+								<TableHeadCell>JOSEP GURDIOLA</TableHeadCell>
+							</TableHead>
+
+							<TableBody tableBodyClass="divide-y">
+								<TableBodyRow>
+									<TableBodyCell>Index Number</TableBodyCell>
+									<TableBodyCell>908978/008</TableBodyCell>
+								</TableBodyRow>
+								<TableBodyRow>
+									<TableBodyCell>PROGRAMME</TableBodyCell>
+									<TableBodyCell>General Arts</TableBodyCell>
+								</TableBodyRow>
+								<TableBodyRow>
+									<TableBodyCell>CLASS</TableBodyCell>
+									<TableBodyCell>General Arts (Geography)</TableBodyCell>
+								</TableBodyRow>
+								<TableBodyRow>
+									<TableBodyCell>Gender</TableBodyCell>
+									<TableBodyCell>Male</TableBodyCell>
+								</TableBodyRow>
+								<TableBodyRow>
+									<TableBodyCell>Residence</TableBodyCell>
+									<TableBodyCell>Boarding</TableBodyCell>
+								</TableBodyRow>
+								<TableBodyRow>
+									<TableBodyCell>Aggretate</TableBodyCell>
+									<TableBodyCell>12</TableBodyCell>
+								</TableBodyRow>
+								<TableBodyRow>
+									<TableBodyCell>Row score</TableBodyCell>
+									<TableBodyCell>780</TableBodyCell>
+								</TableBodyRow>
+								<TableBodyRow>
+									<TableBodyCell>Admission No.</TableBodyCell>
+									<TableBodyCell>PSHS/23/0001</TableBodyCell>
+								</TableBodyRow>
+							</TableBody>
+						</Table>
+					</div>
+					<img class="max-h-[20vh]" src={'/student.png'} alt="Student's Picture" />
+				</div>
+				<Button class="w-fit mt-2 ml-2" color="green">Confirm</Button>
+			</div>
+		{/if}
 	</div>
 </div>
