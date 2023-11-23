de738c3ca24c1f18116f330960ba3acf01092fe0
harshdev2 <averageworkspace@gmail.com>
2023-11-23 14:07:18 +0530

    committ

--- src/routes/placement/form/[id]/+page.svelte
+++ src/routes/placement/form/[id]/+page.svelte
@@ -15,9 +15,7 @@
 		Checkbox,
 		Radio,
 		RadioButton,
-
 		Fileupload
-
 	} from 'flowbite-svelte';
 	import { onMount } from 'svelte';
 	let number = '';
@@ -122,7 +120,7 @@
 			{#if formData.haveMedicalCondition}
 				<Label class="space-y-2 flex flex-col gap-2">
 					Medical Condition
-					<Fileupload bind:files={formData.medicalCondition}/>
+					<Fileupload bind:files={formData.medicalCondition} />
 				</Label>
 			{/if}
 
@@ -257,10 +255,71 @@
 			<!-- BECE Year -->
 			<Label class="space-y-2 flex flex-col gap-2">
 				BECE Year
-				<Input
-					class="text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
-					type="text"
-					name="beceYear"
+				<Select
+					defaultClass="text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500 "
+					items={[
+						{ value: 2030, name: '2030' },
+						{
+							value: 2029, name: '2029'
+						},
+						{
+							value: 2028, name: '2028'
+						},
+						{
+							value: 2027,name: '2027'
+						},
+						{
+							value: 2026, name: '2026'
+						},
+						{
+							value: 2025, name: '2025'
+						},
+						{
+							value: 2024, name: '2024'
+						},
+						{
+							value: 2023, name: '2023'
+						},
+						{
+							value: 2022, name: '2022'
+						},
+						{
+							value: 2021, name: '2021'
+						},
+						{
+							value: 2020, name: '2020'
+						},
+						{
+							value: 2019, name: '2019'
+						},
+						{
+							value: 2018, name: '2018'
+						},
+						{
+							value: 2017, name: '2017'
+						},
+						{
+							value: 2016, name: '2016'
+						},
+						{
+							value: 2015, name: '2015'
+						},
+						{
+							value: 2014, name: '2014'
+						},
+						{
+							value: 2013, name: '2013'
+						},
+						{
+							value: 2012, name: '2012'
+						},
+						{
+							value: 2011, name: '2011'
+						},
+						{
+							value: 2010, name: '2010'
+						}
+					]}
 				/>
 			</Label>
 
